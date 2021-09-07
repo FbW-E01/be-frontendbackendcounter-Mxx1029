89569b221fb58f1517c8b3928b94c4b1c0d70a2b
Mxx1029 <lia.pim@icloud.com>
2021-09-07 10:07:16 +0200

    Add Joel's solution

--- backend/server.js
+++ backend/server.js
@@ -10,12 +10,21 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send(`${counter}`);
+    // Joel's version:
+    // will be visible in console tab in frontend browser
+    // res.send("Hi from the backend");
+    // res.send(counter.toString());
 });
 
 app.post('/', (req, res) => {
     counter++;
     console.log(`${counter}`);
     // res.send(`${counter}`);
+    // Joel's version:
+    // will be visible in console tab in frontend browser
+    // res.send("Hi from the backend")
+    // counter++;
+    // res.send(counter.toString());
 });
 
 app.listen(3005, () => console.log("Server started on http://localhost:3005"))
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ const App = () => {
     const [ state, setState ] = useState();
 
     const handleClickPostButton = () => {
+        // tries to fetch (post sth. but has no data) from our backend (server is running at port 3005)
         const url = 'http://localhost:3005/'
         const options = {
             headers: { "Content-Type" : "application/json"},
@@ -13,14 +14,26 @@ const App = () => {
         }
         fetch(url, options);
         console.log("POST button was clicked!");
+        // Joel's version:
+        // const init = { method: "POST" }
+        // fetch('http://localhost:3005/', init)
+        //     .then(result => result.text())
+        //     .then(text => console.log(text))
+        //     .catch(error => console.error(error))
     }
 
     const handleClickGetButton = () => {
+        // tries to fetch (get data) from our backend (server is running at port 3005)
         const url = 'http://localhost:3005/';
         fetch(url)
             .then(res => res.json())
             .then(x => setState(x))
         console.log("GET button was clicked!");
+        // Joel's version:
+        // fetch('http://localhost:3005/')
+        //     .then(result => result.text())
+        //     .then(text => console.log(text))
+        //     .catch(error => console.error(error))
     }
 
     return (
